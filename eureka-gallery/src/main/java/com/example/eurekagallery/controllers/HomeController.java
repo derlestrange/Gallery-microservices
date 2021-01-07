108578f372427e54a47987df1b487020acf580f3
derlestrange <56831326+derlestrange@users.noreply.github.com>
2021-01-08 00:01:16 +0400

    gallery service

---
+++ eureka-gallery/src/main/java/com/example/eurekagallery/controllers/HomeController.java
@@ -0,0 +1,55 @@
+package com.example.eurekagallery.controllers;
+
+import com.example.eurekagallery.entities.Gallery;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private Environment environment;
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello from Gallery Service running at port: " + environment.getProperty("local.server.port");
+    }
+
+    @HystrixCommand(fallbackMethod = "fallback")
+    @RequestMapping("/{id}")
+    public Gallery getGallery(@PathVariable final int id){
+        LOGGER.info("Creating gallery obj");
+
+        Gallery gallery = new Gallery();
+        gallery.setId(id);
+
+        List<Object> images =  restTemplate.getForObject("http://image-service/images/", List.class);
+        gallery.setImages(images);
+
+        return gallery;
+    }
+
+    @RequestMapping("/admin")
+    public String homeAdmin() {
+        return "This is the admin area of Gallery service running at port: " + environment.getProperty("local.server.port");
+    }
+
+    // a fallback method to be called if failure happened
+    public Gallery fallback(int galleryId, Throwable hystrixCommand) {
+        return new Gallery(galleryId);
+    }
+}

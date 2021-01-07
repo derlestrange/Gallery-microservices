5069d6c5c54a81e263bd85c488684db81e4f2346
derlestrange <56831326+derlestrange@users.noreply.github.com>
2021-01-07 15:59:12 +0400

    init image client

---
+++ eureka-image/src/test/java/com/example/eurekaimage/EurekaImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
